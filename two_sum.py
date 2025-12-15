c99bc861ba717ed335ec668ec95d88be64833704
Leandro Trabucco <leandrotrabucco@hotmail.com>
2025-12-15 23:23:08 +0900

    solved

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,11 @@
 def two_sum(nums, target):
-
+    seen = {}
+    for index, num in enumerate(nums):
+        looking_for = target - num
+        if looking_for in seen:
+            return [seen[looking_for], index]
+        else:
+            seen[num] = index
     pass
 
 # Test
